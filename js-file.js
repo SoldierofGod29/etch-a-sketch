f7a6b24c36a9ad99fdc3067e725f4c912dbccd5d
SoldierofGod29 <nzollers@hotmail.com>
2024-02-23 17:24:06 -0600

    Add event listener to button that prompts user for grid size, removes old grid, and creates new grid size

--- js-file.js
+++ js-file.js
@@ -2,6 +2,7 @@
 
 
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
 let i;
 
@@ -14,8 +15,29 @@ for (i = 0; i < 256; i++)
     div.addEventListener("mouseover", ()=>{
         div.setAttribute('style', 'background: blue');
     });
-
 }
+
+button.addEventListener('click', ()=>{
+    let userInput = prompt("Please enter how big you would like the Grid (Maximum of 100)");
+
+    let j;
+    let totalSquares = Number(userInput) * Number (userInput);
+
+    while (container.firstChild)
+    {
+        container.removeChild(container.firstChild);
+    }
+    
+
+    for (j = 0; j < totalSquares; j++)
+    {
+        let newDiv = document.createElement('div');
+
+        container.appendChild(newDiv);
+    }
+    
+});
+
 
 
 
